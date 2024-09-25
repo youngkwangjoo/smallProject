eb7a9fa5dd962f3a7a4e6cc1ec8dd3efb2b4994e
youngkwangjoo <dudrknd1642@gmail.com>
2024-09-25 14:46:36 +0900

    cors 설정

--- nursesTimetable/nursestimetable/nursesscheduler/settings.py
+++ nursesTimetable/nursestimetable/nursesscheduler/settings.py
@@ -38,9 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'timetable',
+    'corsheaders',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # 프론트엔드 URL
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
