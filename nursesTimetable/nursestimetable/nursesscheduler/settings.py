633d77c6d063345001f4d69042a83770ead82bc8
youngkwangjoo <dudrknd1642@gmail.com>
2024-09-25 15:05:32 +0900

    EC2 접근 설정

--- nursesTimetable/nursestimetable/nursesscheduler/settings.py
+++ nursesTimetable/nursestimetable/nursesscheduler/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&*dsv@_1y-6r*980f4$oza9zwqph$=2c%^1)l%(**94_(t9dub
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
