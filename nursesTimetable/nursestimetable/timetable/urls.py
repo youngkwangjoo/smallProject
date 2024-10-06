034721e434ca066e7dfc6461768436703cff38b6
youngkwangjoo <dudrknd1642@gmail.com>
2024-10-06 23:06:49 +0900

    url 수정

--- nursesTimetable/nursestimetable/timetable/urls.py
+++ nursesTimetable/nursestimetable/timetable/urls.py
@@ -5,5 +5,5 @@ from .views import delete_nurses
 
 urlpatterns = [
     path('generate_schedule/', views.generate_schedule, name='generate_schedule'),  # 스케줄 생성 API
-    path('delete/', delete_nurses, name='delete_nurses'),
+    path('nurses/delete/', delete_nurses, name='delete_nurses'),
 ]
